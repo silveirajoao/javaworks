18d60ed705700482c7709f0673f52d45080c72cd
Joao Silveira <79945769+silveirajoao@users.noreply.github.com>
2021-03-10 19:21:23 -0300

    Add files via upload

---
+++ View/Livro.java
@@ -0,0 +1,44 @@
+package View;
+
+
+public class Livro 
+{
+    
+    public String titulo, genero;
+    private double preco;
+    
+    
+    public Livro() 
+    {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.preco = preco;
+    }
+
+    
+    public void setPreco(double preco) 
+    {
+        this.preco = preco;
+    }
+  
+    public double getPreco() 
+    {
+        return preco;
+    }
+    
+    public String verificarCompra()
+    {
+    
+        if (this.preco >= 0.80)
+        {
+            return "Parabéns, você ganhou 20% de desconto, o valor final da compra foi de: " + (this.preco)*0.80;
+        } 
+    
+        else
+        {
+            return "Que pena, você não ganhou desconto, o valor final da compra foi de: " + this.preco;
+        }
+    
+    } 
+    
+}
